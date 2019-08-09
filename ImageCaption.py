42e74c58882238b343608ba9f58aa0629f0795ac
Harnoor Singh <imnoorsingh@gmail.com>
2019-08-09 21:00:55 +0530

    convert ipynb to py

---
+++ ImageCaption.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from collections import Counter
+from string import digits
+from keras.preprocessing import sequence
+import pickle
+
+
+# In[4]:
+
+
+def build_vocab(sentences,threshold):
+    new_descrip=[]
+    counter=Counter()
+    for line in sentences:
+        #tokenize sentence into words
+        token=line.split()
+        #lowercase words in list
+        low_word=[word.lower() for word in token]
+        #remove digits from the list
+        remove_digits = str.maketrans('', '', digits)
+        word_list= [i.translate(remove_digits) for i in low_word]
+        #remove empty string elements from the list
+        remove_space=list(filter(None,word_list))
+        #update values in counter
+        counter.update(remove_space)
+        #make new list of cleaned descriptions
+        new_descrip.append(" ".join(remove_space))
+    #remove words from vocabulary if frequency is less than given threshold    
+    vocabulary=[key for key,value in counter.items() if value>=threshold]    
+        
+    return vocabulary,new_descrip
+        
+
+
+# In[5]:
+
+
+def max_sequence(description): 
+    #find description with maximum length
+    max_len=0
+    for i in description:
+        sent=i.split()
+        if len(sent)>max_len:
+            max_len=len(sent)  
+    return max_len        
+            
+
+
+# In[6]:
+
+
+def data_generator(batch_size,pd_frame,word2id,clean_desc):
+    images=[]
+    partial_seq=[]
+    next_word=[]
+    batch_count=0
+    while True:
+    
+        for idx in range(len(pd_frame)):
+            encoded_image=encoded_img[pd_frame[idx][0]]
+            encoded_txt=[word2id[text] for text in clean_desc[idx].split()]
+            for i in range(1,len(encoded_txt)):
+                batch_count+=1
+                partial_seq.append(encoded_txt[:i])
+                next_words.append(encoded_txt[i])
+                images.append(encoded_image)
+                
+                if batch_count==batch_size:
+                    batch_count=0
+                    partial_seq=sequence.pad_sequences(partial_seq,max_len,padding="post")#returns zero padded sequence
+                    hotvector = np.zeros([len(next_words), vocab_size])
+                    for i,next_word in enumerate(next_words):
+                        hotvector[i,next_word]=1
+                    images=np.asarray(images)
+                    next_words=np.asarray(hotvector)
+                     
+                    yield [[images,partial_seq],next_words]    
+                    partial_seq=[]
+                    next_words=[]
+                    images=[]
+                
+                    
+                
+
